869eb59d45f38772bb4f5652d61033f97271cfe0
ZahwaAnritia <anritia15@gmail.com>
2025-11-27 17:10:01 +0700

    tambah extension function untuk konversi DetailSiswa

--- app/src/main/java/com/example/a116_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a116_roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,5 +1,21 @@
 package com.example.a116_roomdatabase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
 
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
