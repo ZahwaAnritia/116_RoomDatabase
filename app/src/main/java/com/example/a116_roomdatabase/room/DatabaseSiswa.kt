56a6b592ea6f6714c6fae89b4c7555720b9198ee
ZahwaAnritia <anritia15@gmail.com>
2025-11-27 16:15:15 +0700

    tambah akses DAO dan instance singleton

--- app/src/main/java/com/example/a116_roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/a116_roomdatabase/room/DatabaseSiswa.kt
@@ -12,5 +12,12 @@ import androidx.room.RoomDatabase
 )
 abstract class DatabaseSiswa : RoomDatabase() {
 
+    abstract fun siswaDao(): SiswaDao
 
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+
+    }
 }
