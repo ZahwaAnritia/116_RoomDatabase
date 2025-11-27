1cfd48177dafb70289df37599fb14d81ed272548
ZahwaAnritia <anritia15@gmail.com>
2025-11-27 17:11:53 +0700

    tambah EntryViewModel dengan validasi input Siswa

--- app/src/main/java/com/example/a116_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a116_roomdatabase/viewmodel/EntryViewModel.kt
@@ -7,6 +7,19 @@ import androidx.lifecycle.ViewModel
 import com.example.a116_roomdatabase.repositori.RepositoriSiswa
 import com.example.a116_roomdatabase.room.Siswa
 
+class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa
+            .detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
