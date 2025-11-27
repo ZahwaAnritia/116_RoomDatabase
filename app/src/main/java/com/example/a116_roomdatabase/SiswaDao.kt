63b750d870322d71972ab5eaba8a2c834cebc189
ZahwaAnritia <anritia15@gmail.com>
2025-11-27 16:02:50 +0700

    menambahkan DAO untuk entity Siswa

---
+++ app/src/main/java/com/example/a116_roomdatabase/SiswaDao.kt
@@ -0,0 +1,15 @@
+import  androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.a116_roomdatabase.room.Siswa
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert (onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+}
