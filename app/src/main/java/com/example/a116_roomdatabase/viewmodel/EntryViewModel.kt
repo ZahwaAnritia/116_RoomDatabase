a6befee6dfbc4b7223ac3ff896c5d1404ee7bd85
ZahwaAnritia <anritia15@gmail.com>
2025-11-27 17:09:15 +0700

    tambah extension function untuk konversi Siswa ke DetailSiswa

---
+++ app/src/main/java/com/example/a116_roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.a116_roomdatabase.viewmodel
+
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
